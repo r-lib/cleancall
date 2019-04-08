f82a798a44b70afaeeb72f4454ce616f10fb60f6
Gábor Csárdi <csardi.gabor@gmail.com>
2019-04-08 16:17:40 +0100

    Add public header file (#4)
    Other packages can specify cleancall in `LinkingTo` _and_ `Imports`,
    and then include the `cleancall.h` header.

---
+++ inst/include/cleancall.h
@@ -0,0 +1,34 @@
+#ifndef R_CLEANCALL_H
+#define R_CLEANCALL_H
+
+
+static R_INLINE SEXP r_with_cleanup_context(SEXP (*fn)(void* data),
+                                            void* data) {
+  static SEXP (*cleancall_ptr)(SEXP (*fn)(void*), void *data) = NULL;
+  if (cleancall_ptr == NULL) {
+    cleancall_ptr = (SEXP (*)(SEXP (*)(void *), void *))
+      R_GetCCallable("cleancall", "r_with_cleanup_context");
+  }
+  return cleancall_ptr(fn, data);
+}
+
+static R_INLINE void r_call_on_exit(void (*fn)(void* data), void* data) {
+  static void (*cleancall_ptr)(void (*fn)(void*), void *data) = NULL;
+  if (cleancall_ptr == NULL) {
+    cleancall_ptr = (void (*)(void (*)(void *), void *))
+      R_GetCCallable("cleancall", "r_call_on_exit");
+  }
+  cleancall_ptr(fn, data);
+}
+
+static R_INLINE void r_call_on_early_exit(void (*fn)(void* data),
+                                          void* data) {
+  static void (*cleancall_ptr)(void (*fn)(void*), void *data) = NULL;
+  if (cleancall_ptr == NULL) {
+    cleancall_ptr = (void (*)(void (*)(void *), void *))
+      R_GetCCallable("cleancall", "r_call_on_early_exit");
+  }
+  cleancall_ptr(fn, data);
+}
+
+#endif
