debbc06461cb2409c383661fe384664f7deb803a
Gábor Csárdi <csardi.gabor@gmail.com>
2020-01-10 13:21:52 +0000

    Fix invalid reads in test cases
    You cannot register an exit handler on data from
    the stack.

--- src/test.c
+++ src/test.c
@@ -13,22 +13,38 @@ static void cb(void* data_) {
   SETCDR(data->node, Rf_cons(data->value, CDR(data->node)));
 }
 
-SEXP test_callback_return(SEXP node) {
-  struct cb_data data1 = { node, Rf_install("first") };
-  r_call_on_exit(cb, (void*) &data1);
+struct cb_data *make_data1(SEXP node) {
+  struct cb_data *data1 =
+    (struct cb_data*) R_alloc(1, sizeof(struct cb_data));
+  data1->node = node;
+  data1->value = Rf_install("first");
+  return data1;
+}
 
-  struct cb_data data2 = { node, Rf_install("second") };
-  r_call_on_exit(cb, (void*) &data2);
+struct cb_data *make_data2(SEXP node) {
+  struct cb_data *data2 =
+    (struct cb_data*) R_alloc(1, sizeof(struct cb_data));
+  data2->node = node;
+  data2->value = Rf_install("second");
+  return data2;
+}
+
+SEXP test_callback_return(SEXP node) {
+  struct cb_data *data1 = make_data1(node);
+  r_call_on_exit(cb, (void*) data1);
+
+  struct cb_data *data2 = make_data2(node);
+  r_call_on_exit(cb, (void*) data2);
 
   return R_NilValue;
 }
 
 SEXP test_callback_jump(SEXP node) {
-  struct cb_data data1 = { node, Rf_install("first") };
-  r_call_on_exit(cb, (void*) &data1);
+  struct cb_data *data1 = make_data1(node);
+  r_call_on_exit(cb, (void*) data1);
 
-  struct cb_data data2 = { node, Rf_install("second") };
-  r_call_on_exit(cb, (void*) &data2);
+  struct cb_data *data2 = make_data2(node);
+  r_call_on_exit(cb, (void*) data2);
 
   Rf_error("tilt");
 } /* # nocov (we never get here) */
@@ -40,11 +56,11 @@ static void jumpy_cb(void* data_) {
 SEXP test_jumpy_callback(SEXP node) {
   r_call_on_exit(jumpy_cb, NULL);
 
-  struct cb_data data1 = { node, Rf_install("first") };
-  r_call_on_exit(cb, (void*) &data1);
+  struct cb_data *data1 = make_data1(node);
+  r_call_on_exit(cb, (void*) data1);
 
-  struct cb_data data2 = { node, Rf_install("second") };
-  r_call_on_exit(cb, (void*) &data2);
+  struct cb_data *data2 = make_data2(node);
+  r_call_on_exit(cb, (void*) data2);
 
   Rf_error("tilt");
 } /* # nocov (we never get here)*/
@@ -54,31 +70,31 @@ SEXP test_no_callbacks() {
 }
 
 SEXP test_early_ok(SEXP node) {
-  struct cb_data data1 = { node, Rf_install("first") };
-  r_call_on_early_exit(cb, (void*) &data1);
+  struct cb_data *data1 = make_data1(node);
+  r_call_on_early_exit(cb, (void*) data1);
 
-  struct cb_data data2 = { node, Rf_install("second") };
-  r_call_on_early_exit(cb, (void*) &data2);
+  struct cb_data *data2 = make_data2(node);
+  r_call_on_early_exit(cb, (void*) data2);
 
   return R_NilValue;
 }
 
 SEXP test_early_jump(SEXP node) {
-  struct cb_data data1 = { node, Rf_install("first") };
-  r_call_on_early_exit(cb, (void*) &data1);
+  struct cb_data *data1 = make_data1(node);
+  r_call_on_early_exit(cb, (void*) data1);
 
-  struct cb_data data2 = { node, Rf_install("second") };
-  r_call_on_early_exit(cb, (void*) &data2);
+  struct cb_data *data2 = make_data2(node);
+  r_call_on_early_exit(cb, (void*) data2);
 
   Rf_error("jump");
 } /* # nocov (we never get here) */
 
 SEXP test_mixed(SEXP node) {
-  struct cb_data data1 = { node, Rf_install("first") };
-  r_call_on_exit(cb, (void*) &data1);
+  struct cb_data *data1 = make_data1(node);
+  r_call_on_exit(cb, (void*) data1);
 
-  struct cb_data data2 = { node, Rf_install("second") };
-  r_call_on_early_exit(cb, (void*) &data2);
+  struct cb_data *data2 = make_data2(node);
+  r_call_on_early_exit(cb, (void*) data2);
 
   return R_NilValue;
 }
