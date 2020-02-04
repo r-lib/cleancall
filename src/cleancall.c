0988e82c13c9162a564bdf6f4fe96c54f1423861
Gábor Csárdi <csardi.gabor@gmail.com>
2020-02-04 23:07:44 +0000

    Merge invoke.c to main C file

--- src/cleancall.c
+++ src/cleancall.c
@@ -32,6 +32,30 @@ void cleancall_SetExternalPtrAddrFn(SEXP s, DL_FUNC p) {
 }
 
 
+// Initialised at load time with the `.Call` primitive
+extern SEXP fns_dot_call;
+
+struct eval_args {
+  SEXP call;
+  SEXP env;
+};
+
+static SEXP eval_wrap(void* data) {
+  struct eval_args* args = (struct eval_args*) data;
+  return Rf_eval(args->call, args->env);
+}
+
+SEXP cleancall_call(SEXP args, SEXP env) {
+  SEXP call = PROTECT(Rf_lcons(fns_dot_call, args));
+  struct eval_args data = { call, env };
+
+  SEXP out = r_with_cleanup_context(&eval_wrap, &data);
+
+  UNPROTECT(1);
+  return out;
+}
+
+
 static SEXP callbacks = NULL;
 
 // Preallocate a callback
